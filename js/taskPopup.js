878a9701047b2b303d269d99b8bc6233063f4bef
Sergey <sergolskiy@gmail.com>
2021-04-25 23:06:14 +0300

    fix

--- js/taskPopup.js
+++ js/taskPopup.js
@@ -16,38 +16,9 @@ function changeTypeTask(type, that) {
     if($(that).hasClass('active')){
         $('.taskPopup-canter-table tr[data-type="'+type+'"]').hide()
         $('.taskPopup-canter-table tr[data-type="'+type+'"]').prev().hide()
-        // switch (type) {
-        //     case 'track':
-        //         $('.taskPopup-canter-table tr[data-type="track"]').hide()
-        //         break
-        //     case 'notif':
-        //         $('.taskPopup-canter-table tr[data-type="notif"]').hide()
-        //         break
-        //     case 'task':
-        //         $('.taskPopup-canter-table tr[data-type="task"]').hide()
-        //         break
-        //     case 'recip':
-        //         $('.taskPopup-canter-table tr[data-type="recip"]').hide()
-        //         break
-        // }
     } else {
         $('.taskPopup-canter-table tr[data-type="'+type+'"]').show()
         $('.taskPopup-canter-table tr[data-type="'+type+'"]').prev().show()
-        // switch (type) {
-        //     case 'track':
-        //         $('.taskPopup-canter-table tr[data-type="track"]').show()
-        //         break
-        //     case 'notif':
-        //         $('.taskPopup-canter-table tr[data-type="notif"]').show()
-        //         break
-        //     case 'task':
-        //         $('.taskPopup-canter-table tr[data-type="task"]').show()
-        //         break
-        //     case 'recip':
-        //         $('.taskPopup-canter-table tr[data-type="recip"]').show()
-        //         break
-        // }
-        // $('.taskPopup-canter-table tr[data-type="track"]').prev().show()
     }
 
 }
