18d2f454327e9e8da6b1db217473c532f646b8dd
Sergey <sergolskiy@gmail.com>
2021-04-28 22:29:35 +0300

    fix

--- js/taskPopup.js
+++ js/taskPopup.js
@@ -38,3 +38,118 @@ function changeCreateType(that, type) {
     $('.taskPopup-create-task').removeClass('active')
     $('.taskPopup-create-task[data-task="'+type+'"]').addClass('active')
 }
+
+$(document).ready(function () {
+    let today = moment()
+    let lastDay = moment(today.format('MM/DD/YYYY'), "MM/DD/YYYY").add(10, 'days');
+
+// $('.selected-data').html(moment().format('DD/MM/YYYY'))
+// $('.selected-day').html(moment().format('dddd'))
+
+    var picker = $('.daterange-input').daterangepicker({
+        "singleDatePicker": true,
+        "linkedCalendars": false,
+        "autoUpdateInput": true,
+        "showCustomRangeLabel": false,
+        "autoApply": true,
+        "startDate": today.format('MM/DD/YYYY'),
+        "minDate": today.format('MM/DD/YYYY'),
+        "maxDate": lastDay.format('MM/DD/YYYY'),
+
+        locale: {
+            "format": 'MM/DD/YYYY',
+            "applyLabel": "Ок",
+            "cancelLabel": "Отмена",
+            "fromLabel": "От",
+            "toLabel": "До",
+            "customRangeLabel": "Произвольный",
+            "daysOfWeek": [
+                "Вс",
+                "Пн",
+                "Вт",
+                "Ср",
+                "Чт",
+                "Пт",
+                "Сб"
+            ],
+            "monthNames": [
+                "Январь",
+                "Февраль",
+                "Март",
+                "Апрель",
+                "Май",
+                "Июнь",
+                "Июль",
+                "Август",
+                "Сентябрь",
+                "Октябрь",
+                "Ноябрь",
+                "Декабрь"
+            ],
+            "firstDay": 1
+        }
+    });
+
+    $('.daterange-input').val('')
+
+    $('.daterangepicker').addClass('no-active')
+
+    picker.on('apply.daterangepicker', function(ev, picker) {
+        let money = $('.daterange-input[data-money]').attr('data-money')
+
+        $('.daterangepicker').removeClass('no-active')
+        $('.daterange-input').val(picker.startDate.format('DD/MM/YYYY'))
+
+        if(today.format('YYYY-MM-DD') === picker.startDate.format('YYYY-MM-DD')){
+            $('.select-money').html(money)
+        } else {
+            let diff = picker.startDate.diff(today, 'day') + 2
+            $('.select-money').html(diff * parseInt(money))
+        }
+
+    });
+
+    var picker2 = $('.daterange-input-2').daterangepicker({
+        "singleDatePicker": true,
+        "linkedCalendars": false,
+        "autoUpdateInput": true,
+        "showCustomRangeLabel": false,
+        "autoApply": true,
+        "startDate": today.format('MM/DD/YYYY'),
+        "minDate": today.format('MM/DD/YYYY'),
+        "maxDate": lastDay.format('MM/DD/YYYY'),
+
+        locale: {
+            "format": 'MM/DD/YYYY',
+            "applyLabel": "Ок",
+            "cancelLabel": "Отмена",
+            "fromLabel": "От",
+            "toLabel": "До",
+            "customRangeLabel": "Произвольный",
+            "daysOfWeek": [
+                "Вс",
+                "Пн",
+                "Вт",
+                "Ср",
+                "Чт",
+                "Пт",
+                "Сб"
+            ],
+            "monthNames": [
+                "Январь",
+                "Февраль",
+                "Март",
+                "Апрель",
+                "Май",
+                "Июнь",
+                "Июль",
+                "Август",
+                "Сентябрь",
+                "Октябрь",
+                "Ноябрь",
+                "Декабрь"
+            ],
+            "firstDay": 1
+        }
+    });
+});
